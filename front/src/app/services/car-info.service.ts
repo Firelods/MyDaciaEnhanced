59dcfb05a819be26eafc914c770e61e57eb1e0d3
Firelods <clement.lefevre1@etu.unice.fr>
2024-01-13 22:36:32 +0100

    🤡 Implement mock to bypass renault api call

--- front/src/app/services/car-info.service.ts
+++ front/src/app/services/car-info.service.ts
@@ -3,6 +3,7 @@ import { CarInfo } from '../interfaces/car-info';
 import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environment';
+import { MOCK_CAR_INFO } from './mock/car-info';
 
 @Injectable({
   providedIn: 'root',
@@ -11,44 +12,9 @@ export class CarInfoService {
   constructor(private http: HttpClient) {}
 
   getCarInfo(): Observable<CarInfo> {
-    // return this.http.get<CarInfo>(environment.baseUrl + '/car_info');
-    return of({
-      autonomy: 207,
-      batteryLevel: 97,
-      charging: false,
-      climate: false,
-      history: [
-        {
-          action: 'charge',
-          created_at: new Date('2024-01-06T17:10:59.000Z'),
-          informations: '',
-          login_id: 'contact@clement-lefevre.fr',
-          success: true,
-        },
-        {
-          action: 'charge',
-          created_at: new Date('2024-01-06T17:22:01.000Z'),
-          informations: '',
-          login_id: 'contact@clement-lefevre.fr',
-          success: true,
-        },
-        {
-          action: 'charge',
-          created_at: new Date('2024-01-06T18:10:02.000Z'),
-          informations: '',
-          login_id: 'contact@clement-lefevre.fr',
-          success: true,
-        },
-      ],
-      imageUrl:
-        'https://static.renault.co.uk/cms/version/2021-01/renault-logo-2020.png',
-      lastRefresh: new Date('2024-01-06T14:13:07Z'),
-      name: 'SPRING',
-      scheduled: {
-        airConditioning: { type: 'ac' },
-        charging: { type: 'charge' },
-      },
-      totalKilometers: 3366.88,
-    });
+    if (environment.mock) {
+      return of(MOCK_CAR_INFO);
+    }
+    return this.http.get<CarInfo>(environment.baseUrl + '/car_info');
   }
 }
