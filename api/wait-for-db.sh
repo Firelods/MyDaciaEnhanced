2c5f0d2070db5849df840e9890a60985e59645b7
Firelods <clement.lefevre1@etu.unice.fr>
2024-01-18 22:34:38 +0100

    :zap: Improve docker compose by adding a wait for postgresdb init #17

---
+++ api/wait-for-db.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# wait-for-db.sh
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until nc -z "$host" 5432; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
+exec $cmd
