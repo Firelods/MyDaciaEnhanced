6fef866568104c6cdeb71c43ee29b028648880de
Firelods <clement.lefevre1@etu.unice.fr>
2024-02-11 23:35:22 +0100

    ✨ add chooseVehicle and init_renault_login support #8

--- front/src/app/services/user.service.ts
+++ front/src/app/services/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { environment } from '../environment';
 import { Router } from '@angular/router';
+import { VehicleList } from '../interfaces/vehicle-list';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -26,20 +28,36 @@ export class UserService {
       });
   }
 
-  init_renault_login(email: string, password: string) {
-    this.http
-      .post<{ message: string; token: string }>(
-        environment.baseUrl + '/init_renault_session',
-        { email, password }
-      )
-      .subscribe((response) => {
-        const token = response.token;
-        console.log(response);
+  init_renault_login(
+    email: string,
+    password: string
+  ): Observable<VehicleList[]> {
+    if (environment.mock) {
+      return of([
+        {
+          model: 'Spring',
+          vin: 'VF1RFA00V6V000000',
+          licensePlate: 'AA-000-AA',
+        },
+      ]);
+    }
+    return this.http.post<VehicleList[]>(
+      environment.baseUrl + '/init_renault_session',
+      {
+        email,
+        password,
+      }
+    );
+  }
 
-        if (token) {
-          localStorage.setItem('token', token);
-          this.router.navigate(['/']);
-        }
+  chooseVehicle(vin: string) {
+    this.http
+      .post(environment.baseUrl + '/set_vin', {
+        vin,
+      })
+      .subscribe((response) => {
+        console.log(response);
+        this.router.navigate(['/']);
       });
   }
 }
